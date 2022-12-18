540bbc47b877c07726a33f1532b193cbcdabbb7f
iVampireSP.com <im@ivampiresp.com>
2022-12-18 11:58:22 +0800

    适配新的 API

--- app/Http/Middleware/ApiToken.php
+++ app/Http/Middleware/ApiToken.php
@@ -48,9 +48,9 @@ class ApiToken
                 $user = $http->get('/users/' . $request->user_id)->json();
 
                 $user = User::create([
-                    'id' => $user['data']['id'],
-                    'name' => $user['data']['name'],
-                    'email' => $user['data']['email'],
+                    'id' => $user['id'],
+                    'name' => $user['name'],
+                    'email' => $user['email'],
                 ]);
             }
 
