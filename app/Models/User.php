e877ee71421b82df0d04f366878e00da36533de3
iVampireSP.com <im@ivampiresp.com>
2023-01-18 01:50:59 +0800

    改进

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,8 +11,6 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    // fillable
-    public mixed $id;
     protected $fillable = [
         'id',
         'name',
