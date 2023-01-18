5d76b71b0a7000511197138575a47de7bff0271a
iVampireSP.com <im@ivampiresp.com>
2023-01-19 00:07:10 +0800

    改进 路由

--- routes/remote.php
+++ routes/remote.php
@@ -1,8 +1,12 @@
 <?php
 
 use App\Http\Controllers\Remote\Exports;
-use App\Http\Controllers\Remote\Functions;
+// use App\Http\Controllers\Remote\Functions;
 use Illuminate\Support\Facades\Route;
+
+
+/* 注意，为了性能考虑，不再推荐下方的方式暴露方法。 */
+
 
 // 注意，以下路由都是暴露给用户的，并且必须经过 'Remote' 中间件，否则这些路由将不安全。
 
@@ -13,9 +17,10 @@ use Illuminate\Support\Facades\Route;
  */
 
 // 当前模块的函数。服务器启停，创建，销毁，都需要进过这里。
-Route::group(['prefix' => '/functions', 'as' => 'functions.'], function () {
-    Route::apiResource('hosts', Functions\HostController::class);
-});
+// Route::group(['prefix' => '/functions', 'as' => 'functions.'], function () {
+//     Route::apiResource('hosts', Functions\HostController::class);
+// });
+
 
 // 导出函数。用于给其它集成模块调用。做到模块之间相互交换信息或控制。
 Route::group(['prefix' => '/exports', 'as' => 'exports.'], function () {
