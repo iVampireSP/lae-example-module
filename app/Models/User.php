bd2deba4c832f824fcf83db6103c2cffc7d6d9ee
iVampireSP.com <im@ivampiresp.com>
2023-01-19 00:07:29 +0800

    改进 继承 Cocoa 的模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,18 +2,7 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
-
-class User extends Authenticatable
+class User extends \ivampiresp\Cocoa\Models\User
 {
-    use HasApiTokens, HasFactory, Notifiable;
 
-    protected $fillable = [
-        'id',
-        'name',
-        'email',
-    ];
 }
