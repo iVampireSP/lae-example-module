634d98e7c23e89b50f81b5dc711f5efc92f46226
iVampireSP.com <im@ivampiresp.com>
2023-01-19 01:51:37 +0800

    改进 API

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\HostController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/user', UserController::class);
+Route::resource('hosts', HostController::class);
