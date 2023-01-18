49f856b4e86fb66094f43ed5fc157cd97a6af3c3
iVampireSP.com <im@ivampiresp.com>
2023-01-19 00:14:44 +0800

    优化代码

--- app/View/Components/HostStatus.php
+++ app/View/Components/HostStatus.php
@@ -3,10 +3,11 @@
 namespace App\View\Components;
 
 use Illuminate\View\Component;
+use Illuminate\View\View;
 
 class HostStatus extends Component
 {
-    public $status = null;
+    public string|null $status = null;
 
     /**
      * Create a new component instance.
@@ -15,17 +16,15 @@ class HostStatus extends Component
      */
     public function __construct($status)
     {
-        //
-
         $this->status = $status;
     }
 
     /**
      * Get the view / contents that represent the component.
      *
-     * @return \Illuminate\Contracts\View\View|\Closure|string
+     * @return View
      */
-    public function render()
+    public function render(): View
     {
         return view('components.host-status', ['status' => $this->status]);
     }
