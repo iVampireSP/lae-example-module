a6f46ba39b3c02bc3bd79431cb4a780619f605b4
iVampireSP.com <im@ivampiresp.com>
2023-01-31 01:21:31 +0800

    改进 显示名称

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name') }}</title>
+    <title>{{ config('app.display_name') }}</title>
 
     <!-- Fonts -->
     {{-- <link rel="dns-prefetch" href="//fonts.gstatic.com"> --}}
@@ -23,7 +23,7 @@
     <nav class="navbar navbar-expand-md shadow-sm bg-body">
         <div class="container">
             <a class="navbar-brand" href="{{ route('index') }}">
-                {{ config('app.name') }}
+                {{ config('app.display_name') }}
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
